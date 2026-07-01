25841e677973de806c7e40f05af95a394c7f7929
ashutosh229 <akumarjha875@gmail.com>
2026-07-01 18:42:35 +0530

    feat(app): implement todo fetching and form accessibility

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,25 +1,52 @@
-import './App.css';
-import logo from './logo.svg';
-
+import { useEffect, useState } from "react";
+import "./App.css";
 
 export function App() {
+  const [todos, setTodos] = useState([]);
+  const [description, setDescription] = useState("");
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/todos/");
+      if (!response.ok) {
+        throw new Error("Failed to fetch todos.");
+      }
+      const data = await response.json();
+      setTodos(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
   return (
     <div className="App">
       <div>
         <h1>List of TODOs</h1>
-        <li>Learn Docker</li>
-        <li>Learn React</li>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo._id}>{todo.description}</li>
+          ))}
+        </ul>
       </div>
       <div>
         <h1>Create a ToDo</h1>
         <form>
           <div>
-            <label for="todo">ToDo: </label>
-            <input type="text" />
+            <label htmlFor="todo">ToDo: </label>
           </div>
-          <div style={{"marginTop": "5px"}}>
+          <div style={{ marginTop: "5px" }}>
             <button>Add ToDo!</button>
           </div>
+          <input
+            id="todo"
+            type="text"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
         </form>
       </div>
     </div>
