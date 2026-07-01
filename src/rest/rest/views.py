f8fd6bf38478218a2d35c0f32ce07e81ca195cb7
ashutosh229 <akumarjha875@gmail.com>
2026-07-01 18:55:19 +0530

    refactor(api): improve MongoDB error handling and response consistency

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -1,22 +1,37 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import json, logging, os
-from pymongo import MongoClient
 
-mongo_uri = "mongodb://" + os.environ["MONGO_HOST"] + ":" + os.environ["MONGO_PORT"]
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+
+import os
+
+mongo_uri = f"mongodb://{os.environ['MONGO_HOST']}:{os.environ['MONGO_PORT']}"
 db = MongoClient(mongo_uri)["test_db"]
+todo_collection = db.todos
 
 
 class TodoListView(APIView):
     def get(self, request):
-        todos = list(db.todos.find())
+        try:
+            todos = []
 
-        for todo in todos:
-            todo["_id"] = str(todo["_id"])
+            for todo in todo_collection.find():
+                todos.append(
+                    {
+                        "_id": str(todo["_id"]),
+                        "description": todo["description"],
+                    }
+                )
 
-        return Response(todos, status=status.HTTP_200_OK)
+            return Response(todos, status=status.HTTP_200_OK)
+
+        except PyMongoError:
+            return Response(
+                {"error": "Failed to retrieve TODOs."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
     def post(self, request):
         description = request.data.get("description", "").strip()
@@ -27,16 +42,23 @@ class TodoListView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        result = db.todos.insert_one(
-            {
-                "description": description,
-            }
-        )
+        try:
+            result = todo_collection.insert_one(
+                {
+                    "description": description,
+                }
+            )
 
-        return Response(
-            {
-                "_id": str(result.inserted_id),
-                "description": description,
-            },
-            status=status.HTTP_201_CREATED,
-        )
+            return Response(
+                {
+                    "_id": str(result.inserted_id),
+                    "description": description,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+
+        except PyMongoError:
+            return Response(
+                {"error": "Failed to create TODO."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
